1c4bdd00d07a1add7e4d38ab85fd0af884b98375
Penguin <ad0097@uah.edu>
2019-09-01 19:35:01 -0500

    Added comments, added check for infinite recursive calls in PickupBagAtIndex

--- HW1/main.cpp
+++ HW1/main.cpp
@@ -25,7 +25,6 @@ int main(int argc, char** argv)
 		return -1;
 	}
 
-	
 	oStream.open(outputFile);
 	if (oStream.bad())
 	{
@@ -34,12 +33,14 @@ int main(int argc, char** argv)
 		return -1;
 	}
 
+	// empty all lockers
 	InitializeLockers(lockerBank);
+	// Read locker info inputs, handle bag requests, print out to the output file
 	ReadLockerInfo(lockerBank, iStream, oStream);
 
+	// close file streams
 	iStream.close();
 	oStream.close();
-	
 	return 0;
 }
 // Returns the number of empty lockers in mylockers.
@@ -74,38 +75,50 @@ int FindAvailableLocker(Locker mylockers[])
 // Assigns a locker for personand keeps the key for another person as nextkey for a group of people.
 int DropBag(Locker mylockers[], const char* person, int nextkey = -1)
 {
+	// Get the next available locker index
 	int lockerNum = FindAvailableLocker(mylockers);
+	// Make sure the lockerNum is >= 0
+	// If our lockerNumber is -1, there are no available lockers so we must return
+	// If our lockerNumber is >= our max number of lockers, an error has occured
+	// The second part of the comparison is purely for error checking.
 	if (lockerNum < 0 || lockerNum >= NUM_LOCKERS)
 	{
-		
+		// Return the bad lucker number as -1 to indicate that no lockers were available
 		return lockerNum;
 	}
 	// Fill the locker
 	FillLocker(&mylockers[lockerNum], person, nextkey);
+	// Return the key of the locker the bag was assigned to
 	return lockerNum;
 }
 
-// Find the locker for person in mylockers.
-int GetLocker(Locker mylockers[], const char* person)
+// Find the locker key for person in mylockers.
+int GetLockerKey(Locker mylockers[], const char* person)
 {
-	int retIndex = -1;
+	int retKey = -1;
 	for (int ind = 0; ind < NUM_LOCKERS; ind++)
 	{
+		// If our person argument matches the person assigned to the locker, we have the locker we want
 		if (std::string(person) == std::string(mylockers[ind].name))
 		{
-			retIndex = ind;
+			// Assign our return key as the index
+			retKey = ind;
 			break;
 		}
 	}
-	return retIndex;
+	return retKey;
 }
 
 // Empties the locker for person after finding his or her lockerand the lockers of the people in the group if any.
 void PickupBag(Locker mylockers[], const char* person)
 {
-	int key = GetLocker(mylockers, person);
+	// Get the locker key associated with the person arg supplied
+	int key = GetLockerKey(mylockers, person);
+	// Make sure we found a locker associated with the person
+	// Second part if this check is purely for error checking
 	if (key >= 0 && key < NUM_LOCKERS)
 	{
+		// Pickup the bag at the key found for person
 		PickupBagAtIndex(mylockers, key);
 	}
 	else
@@ -116,21 +129,23 @@ void PickupBag(Locker mylockers[], const char* person)
 
 }
 
-
 // Faster way of running PickupBag
-// After we've found the initial keyholder of a group, we know longer need to search by name because we have all the indexes
-void PickupBagAtIndex(Locker* lockers, int key)
+// After we've found the initial keyholder of a group, we know longer need to search by name because we have all the indexes we need
+void PickupBagAtIndex(Locker lockers[], int key)
 {
+	// Make sure our lockers pointer is not null
 	if (!lockers)
 	{
 		std::cout << "Error" << std::endl;
 		return;
 	}
+	// Make sure the key provided is valid
 	if (key >= NUM_LOCKERS || key < 0)
 	{
-		std::cout << "Error" << std::endl;
+		std::cout << "Error: Key invalid at " << __LINE__ << " " << std::endl;
 		// error
 	}
+	// Make sure the locker is marked as in use
 	else if (!lockers[key].resv)
 	{
 		std::cout << "Error" << std::endl;
@@ -138,16 +153,27 @@ void PickupBagAtIndex(Locker* lockers, int key)
 	}
 	else
 	{
+		// Check if this locker is part of a group
 		if (lockers[key].key >= 0)
 		{
+			// This locker is a part of a group so we need to pick up the bag in the locker using the key being stored in this locker
+			// PickupBagAtIndex is a recursive call when the lockers key is not -1,
+			// so we need to do one check to make sure our stack doesn't overflow
+			// This check is here to prevent crashes in the event of some error on my part
+			if (key == lockers[key].key)
+			{
+				std::cout << "Error: Attempting to recursively call " << __FUNCTION__ << " at " << __LINE__ << " an infinite amount of times" << std::endl;
+				return;
+			}
 			PickupBagAtIndex(lockers, lockers[key].key);
 		}
-		std::cout << "Picking up " << lockers[key].name << "'s bag" << std::endl;
+		//std::cout << "Picking up " << lockers[key].name << "'s bag" << std::endl;
 		EmptyLocker(&lockers[key]);
 	}
 }
 
 // Displays mylockers in a simple formatand writes the result to an output stream named outfile.
+// Function provided by Dr. Aygün
 void DisplayLockers(Locker mylockers[], std::ofstream &outfile)
 {
 	int i, j;
@@ -172,7 +198,6 @@ void DisplayLockers(Locker mylockers[], std::ofstream &outfile)
 			{
 				outfile << " " << i << ")" << std::setw(10) << mylockers[i].name << " . " << std::setw(5) <<  " | ";
 			}
-			
 		}	
 		else
 		{
@@ -190,64 +215,87 @@ void DisplayLockers(Locker mylockers[], std::ofstream &outfile)
 // Reads pickupand drop information from file having name infilenameand assigns or empties the lockers accordingly.
 void ReadLockerInfo(Locker mylockers[], std::ifstream &infile, std::ofstream &outfile)
 {
-	std::cout << "Reading input file\n";
 	while (!infile.eof())
 	{
+		// buff - buffer used to hold lines being read from our file
 		char buff[256];
+		// read a \n terminated line from our input stream
 		infile.getline(buff, 256);
 
 		// Make sure our line was valid
 		if (isalpha(buff[0]))
 		{
+			// String stream to hold each line read from our file
 			std::istringstream ifss(buff);
+			// action - Decides whether our person is picking a bag up or dropping a bag off
+			// Ideally action can only be "drop" or "pickup"
 			std::string action;
-			int nArg = 0;
+			// name - name of the person dropping off or picking up
 			std::string name;
+			// Read the first word from our string stream
 			ifss >> action;
 			if (action == "drop")
 			{
-				ifss >> nArg;
-				// input no. validity check
-				if (nArg >= 1 && nArg <= NUM_LOCKERS)
+				// drop_num - Number of bags being dropped off
+				int drop_num = 0;
+				// Read the next int into drop_num from our string stream
+				ifss >> drop_num;
+				// drop_num validity check
+				if ((drop_num >= 1) && (drop_num <= NUM_LOCKERS))
 				{
+					// Check and make sure we have enough lockers available to support the amount of bags being dropped off
 					int num_lockers_available = FindNoOfAvailableLockers(mylockers);
-					if (num_lockers_available >= nArg)
+					if (num_lockers_available >= drop_num)
 					{
+						// previousKey -- holds the key previously returned by DropBag
 						int previousKey = -1;
-						for (int x = 0; x < nArg; x++)
+						// Start a loop iterating the number of bags we need dropped off
+						for (int x = 0; x < drop_num; x++)
 						{
+							// Read the first name from our string stream
 							ifss >> name;
-							// here we process the bag with the name
-							std::cout << "Dropping off " << name << "'s bag" << std::endl;
-
+							//std::cout << "Dropping off " << name << "'s bag" << std::endl;
+							// DropBag returns the key of the locker the bag was assigned to
+							// If DropBag returns -1, it means our bag was a failure
+							// We should never actually get a -1 return value because we check beforehand whether or not
+							// we can support the number of bags requested.
 							if ((previousKey = DropBag(mylockers, name.c_str(), previousKey)) < 0)
 							{
 								std::cout << "Error Couldn't add bag. There are no bags available." << std::endl;
 							}
 						}
 					}
+					// If we don't have enough lockers available to support the number of bags being dropped off,
+					// we need to print that to the output file
 					else
 					{
-						outfile << "Couldn't add " << nArg << (nArg > 1 ? " bags" : " bag") << ". Only " 
+						outfile << "Couldn't add " << drop_num << (drop_num > 1 ? " bags" : " bag") << ". Only " 
 							<< num_lockers_available << (num_lockers_available > 1 ? " lockers" : " locker") << " available.\n";
 					}
 				}
+				// This error is only thrown if the number of bags to be dropped off read from the file is wrong
+				// We should never get here
 				else
 				{
-					std::cout << "Invalid input. We don't support " << nArg << " lockers." << std::endl;
+					std::cout << "Invalid input. We don't support " << drop_num << " lockers." << std::endl;
 				}
 			}
 			else if (action == "pickup")
 			{
+				// Read the name of the person picking up their bag from the string stream
 				ifss >> name;
+				// Picks up bag for the person requested
+				// If the person is a part of a group, it will handle those bags as well
 				PickupBag(mylockers, name.c_str());
 			}
+			// if we get here, it means there was an error reading from the input file. 
+			// Make sure the input file is correct
 			else
 			{
 				// error unknown action input
 			}
+			// After a successful bag transaction (either a pickup or drop off) we print our table of bags and people to the output file
 			DisplayLockers(mylockers, outfile);
-			std::cout << std::endl;
 		}
 	}
 }
@@ -260,7 +308,6 @@ void InitializeLockers(Locker mylockers[])
 		EmptyLocker(&mylockers[ind]);
 	}
 }
-
 
 void EmptyLocker(Locker* locker)
 {
